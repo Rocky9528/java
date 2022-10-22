06ed26fc4709f22ab0c2bc06c66d087dc5b8d9a4
Rocky9528 <188165657@qq.com>
2022-10-22 23:37:49 +0800

    202210222337

--- javaframework/spring/01spring框架的介绍及使用/code/spring_study_maven/src/main/java/com/rocky/bean/Person.java
+++ javaframework/spring/01spring框架的介绍及使用/code/spring_study_maven/src/main/java/com/rocky/bean/Person.java
@@ -1,15 +1,27 @@
 package com.rocky.bean;
-public class Person {
-    private int id;
-    private String name;
-    private int age;
-    private String gender;
 
-    public int getId() {
+import java.util.*;
+
+public class Person {
+
+    public  final static String constantName="张三";
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Address address;
+    private String[] hobbies;
+    private List<Book> books;
+    private Set<Integer> sets;
+    private Map<String,Object> maps;
+    private Properties properties;
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -21,11 +33,11 @@ public class Person {
         this.name = name;
     }
 
-    public int getAge() {
+    public Integer getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
@@ -37,6 +49,72 @@ public class Person {
         this.gender = gender;
     }
 
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public Set<Integer> getSets() {
+        return sets;
+    }
+
+    public void setSets(Set<Integer> sets) {
+        this.sets = sets;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public Person() {
+        System.out.println("构造Person");
+    }
+
+    public Person(Integer id, String name, Integer age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Person(Integer id,Integer age, String name,  String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
@@ -44,6 +122,12 @@ public class Person {
                 ", name='" + name + '\'' +
                 ", age=" + age +
                 ", gender='" + gender + '\'' +
+                ", address=" + address +
+                ", hobbies=" + Arrays.toString(hobbies) +
+                ", books=" + books +
+                ", sets=" + sets +
+                ", maps=" + maps +
+                ", properties=" + properties +
                 '}';
     }
 }
--- javaframework/spring/01spring框架的介绍及使用/code/spring_study_maven/src/main/java/com/rocky/test/MyTest.java
+++ javaframework/spring/01spring框架的介绍及使用/code/spring_study_maven/src/main/java/com/rocky/test/MyTest.java
@@ -8,8 +8,34 @@ public class MyTest {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("ioc.xml");
-        Person person = (Person) context.getBean("person");
-        System.out.println(person);
+
+//        Person person3 = context.getBean("person3", Person.class);
+//        System.out.println(person3);
+
+//        Person person5 = context.getBean("person5", Person.class);
+//        System.out.println(person5);
+//
+//        Person person6 = context.getBean("person6", Person.class);
+//        System.out.println(person6);
+
+//        //person7是抽象类，无法实例化
+//        Person person7 = context.getBean("person7", Person.class);
+//        System.out.println(person7);
+
+        Person person8 = context.getBean("person8", Person.class);
+        System.out.println(person8);
+//
+//
+//        Object constant1 = context.getBean("constant1");
+//        System.out.println(constant1);
+//
+//        Object propertypath = context.getBean("propertypath");
+//        System.out.println(propertypath);
+//
+//        Object properties = context.getBean("properties1");
+//        System.out.println(properties);
+
+
     }
 
 }
